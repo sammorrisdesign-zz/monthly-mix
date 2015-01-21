58a50d7f46f99e892012e6fd4f6091974d42cf51
Sam Morris <mail@sammorrisdesign.co.uk>
2015-01-21 00:31:59 +0000

    Add spotify keys and helper file, retrive set playlist

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Monthly-mix
 Something with Spotify and Node
+
+## Installation
+
+Rename `spotify-keys-example.json` to `spotify-keys.json` and enter your spotify api credentials.
--- _backend/playlistFetcher.js
+++ _backend/playlistFetcher.js
@@ -5,6 +5,7 @@ var fs = require("fs");
 
 // Local Modules
 var templates = require("./helpers/templates.js");
+var spotify = require("./helpers/spotify.js");
 
 // External Files
 require.extensions['.html'] = function (module, filename) {
@@ -14,3 +15,27 @@ require.extensions['.html'] = function (module, filename) {
 templates.build('index', {
     test: "hello again"
 });
+
+spotify.init();
+
+/*
+spotify.clientCredentialsGrant()
+    .then(function(data) {
+        spotify.setAccessToken(data['access_token']);
+        console.log(data['access_token']);
+        
+        spotify.getPlaylist('nidzumi', '4z2ECApbbdGi3YTdPhRsNI')
+    .then(function(data) {
+        console.log(data.tracks.items);
+    });
+        
+    })
+spotify.getUser('nidzumi').then(function(data) { console.log(data); });
+
+spotify.getUserPlaylists('nidzumi').then(function(data) { console.log(data); }, function(err) { console.log(err); });
+
+spotify.getPlaylist('nidzumi', '4z2ECApbbdGi3YTdPhRsNI')
+    .then(function(data) {
+        console.log(data);
+    });
+*/
