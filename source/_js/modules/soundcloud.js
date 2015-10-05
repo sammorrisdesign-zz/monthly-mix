08d8294ad1e48ccb1f0280a7358abcf533b8c9d2
Sam Morris <mail@sammorrisdesign.co.uk>
2015-10-05 22:21:35 +0100

    Animate the now playing bar

--- source/_js/modules/soundcloud.js
+++ source/_js/modules/soundcloud.js
@@ -98,14 +98,23 @@ define([
 
         updateNowPlaying: function() {
             track = this.getNowPlayingInfo();
-            bonzo(qwery('.controls__buttons__soundcloud a')).attr('href', track['permalink']);
+
+            // Don't update if the track isn't new
+            if (bonzo(qwery('.post')).attr('data-current-track') == track['id']) {
+                return false;
+            }
+
+            bonzo(qwery('.controls--next .controls__buttons__soundcloud a')).attr('href', track['permalink']);
             bonzo(qwery('.is-changable')).attr("style", track['color'].replace('background-', ''));
             bonzo(qwery('.post')).attr('data-current-track', track['id']);
-            bonzo(qwery('.controls')).removeClass('is-dark is-light is-very-dark is-default').addClass(track['contrast']).attr("style", track['color']);
-            bonzo(qwery('.controls__buttons .input')).attr('style', track['color']);
+            bonzo(qwery('.controls--next')).removeClass('is-dark is-light is-very-dark is-default').addClass(track['contrast']).attr("style", track['color']);
+            bonzo(qwery('.controls--next .controls__buttons .input')).attr('style', track['color']);
             bonzo(qwery('.playlist')).attr("style", track['color'].replace(')', ', 0.2)').replace('rgb', 'rgba'));
-            bonzo(qwery('.controls .controls__title__track-artist')).text(track['artist']);
-            bonzo(qwery('.controls .controls__title__track-title')).text(track['title']);
+            bonzo(qwery('.controls--next .controls__title__track-artist')).text(track['artist']);
+            bonzo(qwery('.controls--next .controls__title__track-title')).text(track['title']);
+            bonzo(qwery('.controls--active')).removeClass("controls--active").addClass("controls--previous");
+            bonzo(qwery('.controls--next')).removeClass("controls--next").addClass("controls--active");
+            bonzo(qwery('.controls--previous')).removeClass("controls--previous").addClass("controls--next");
         },
 
         setPageTitle: function() {
