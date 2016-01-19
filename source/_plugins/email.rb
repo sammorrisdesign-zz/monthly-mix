9424de872d9779c108337c54a8c7734a3d082268
Sam Morris <mail@sammorrisdesign.co.uk>
2016-01-19 00:17:28 +0000

    Add email contents to December playlist, play around with templates to get content into emails

--- source/_plugins/email.rb
+++ source/_plugins/email.rb
@@ -1,23 +1,27 @@
-module Jekyll
-    class EmailPage < Page
-        def initialize(site, base, dir, post)
-            @site = site
-            @base = base
-            @dir = dir
-            @name = 'email.html'
-
-            self.process(@name)
-            self.data = post
-            self.data['layout'] = 'email'
-        end
-    end
-
-    class Email < Generator
-        safe true
-        def generate(site)
-            site.posts.each do |post|
-                site.pages << EmailPage.new(site, site.source, post.url, post.data)
-            end
-        end
-    end
-end
+# module Jekyll
+#     class EmailPage < Page
+#         def initialize(site, base, dir, post, content)
+#             @site = site
+#             @base = base
+#             @dir = dir
+#             @name = 'email.html'
+# 
+#             self.process(@name)
+#             self.data = post
+#             puts content
+#             self.data['content'] = content
+#             puts self.data['content']
+#             self.data['layout'] = 'email'
+#         end
+#     end
+# 
+#     class Email < Generator
+#         safe true
+#         def generate(site)
+#             site.posts.each do |post|
+#                 puts post
+#                 site.pages << EmailPage.new(site, site.source, post.url, post.data, post)
+#             end
+#         end
+#     end
+# end
