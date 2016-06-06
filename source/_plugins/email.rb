c8252ee502c0765f0d4e65b7cecd1472de427527
sammorrisdesign <mail@sammorrisdesign.co.uk>
2016-06-05 23:36:49 -0400

    format email plugin

--- source/_plugins/email.rb
+++ source/_plugins/email.rb
@@ -1,27 +1,27 @@
 module Jekyll
-  class EmailPost < Page
-    def initialize(site, base, dir, post)
-        require 'kramdown'
-      @site = site
-      @base = base
-      @dir = dir
-      @name = 'email.html'
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'email.html')
-      self.data['body'] = Kramdown::Document.new(post.content).to_html.gsub(/\n/, "")
-      self.data['canonical_url'] = post.url
+    class EmailPost < Page
+        def initialize(site, base, dir, post)
+            require 'kramdown'
+            @site = site
+            @base = base
+            @dir = dir
+            @name = 'email.html'
+            self.process(@name)
+            self.read_yaml(File.join(base, '_layouts'), 'email.html')
+            self.data['body'] = Kramdown::Document.new(post.content).to_html
+            self.data['canonical_url'] = post.url
+        end
     end
-  end
 
-  class AmpGenerator < Generator
-    priority :low
-    def generate(site)
-      site.posts.docs.each do |post|
-        index = EmailPost.new(site, site.source, post.id, post)
-        index.render(site.layouts, site.site_payload)
-        index.write(site.dest)
-        site.pages << index
-      end
+    class AmpGenerator < Generator
+        priority :low
+        def generate(site)
+            site.posts.docs.each do |post|
+                index = EmailPost.new(site, site.source, post.id, post)
+                index.render(site.layouts, site.site_payload)
+                index.write(site.dest)
+                site.pages << index
+            end
+        end
     end
-  end
 end
