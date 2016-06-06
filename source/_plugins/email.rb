280d5446f936043530ed34f49e9f1e6d44fe5dc9
sammorrisdesign <mail@sammorrisdesign.co.uk>
2016-06-05 22:28:40 -0400

    email thing

--- source/_plugins/email.rb
+++ source/_plugins/email.rb
@@ -1,27 +1,25 @@
-# module Jekyll
-#     class EmailPage < Page
-#         def initialize(site, base, dir, post, content)
-#             @site = site
-#             @base = base
-#             @dir = dir
-#             @name = 'email.html'
-# 
-#             self.process(@name)
-#             self.data = post
-#             puts content
-#             self.data['content'] = content
-#             puts self.data['content']
-#             self.data['layout'] = 'email'
-#         end
-#     end
-# 
-#     class Email < Generator
-#         safe true
-#         def generate(site)
-#             site.posts.each do |post|
-#                 puts post
-#                 site.pages << EmailPage.new(site, site.source, post.url, post.data, post)
-#             end
-#         end
-#     end
-# end
+module Jekyll
+  class EmailPost < Page
+    def initialize(site, base, dir, post)
+      @site = site
+      @base = base
+      @dir = ''
+      @name = dir + '/email.html'
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'email.html')
+
+    end
+  end
+
+  class AmpGenerator < Generator
+    priority :low
+    def generate(site)
+      site.posts.each do |post|
+        index = EmailPost.new(site, site.source, post.id, post)
+        index.render(site.layouts, site.site_payload)
+        index.write(site.dest)
+        site.pages << index
+      end
+    end
+  end
+end
