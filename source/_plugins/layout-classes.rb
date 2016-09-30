e64ef11c5adbd42c58b4790d6b31a05192463f09
sammorrisdesign <mail@sammorrisdesign.co.uk>
2016-09-30 10:27:16 -0400

    add frames to playlist entries

--- source/_plugins/layout-classes.rb
+++ source/_plugins/layout-classes.rb
@@ -3,7 +3,7 @@
 module Jekyll
     class LayoutClasses < Liquid::Tag
         def render(context)
-            @classes = "left--#{rand(7 - 1)} right--#{rand(7 - 1)} vertical--#{rand(7 - 1)}"
+            @classes = "left--#{rand(7 - 1)} right--#{rand(7 - 1)} vertical--#{rand(7 - 1)} frame--#{rand(6) + 1}"
             if rand(7 - 1) == 1
                 @classes << " clear"
             end
