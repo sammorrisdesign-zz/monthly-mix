e0245523bdd6f5525c19e109e9cd1b5adfeda3cc
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-03-20 21:21:49 -0400

    get basic version of data

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -5,12 +5,13 @@ var youtube = require('youtube-api');
 module.exports = {
     fetch: function() {
         var id = 'PLYR_FLaQpljOY9YTl4UArYVXn12P8BGBB';
-        var data = {};
 
-        this.getPlaylist(id, function(playlist) {
+        this.getPlaylist(id, function(data) {
+            var playlist = this.cleanData(data);
+
             fs.mkdirsSync('.data');
             fs.writeFileSync('.data/february.json', JSON.stringify(playlist));
-        });
+        }.bind(this));
     },
 
     getPlaylistInfo: function(playlistId, callStackSize, pageToken, currentItems, callBack) {
@@ -44,5 +45,26 @@ module.exports = {
         });
 
         this.getPlaylistInfo(id, 0, null, [], done)
+    },
+
+    cleanData: function(data) {
+        var playlist = {};
+        for (var i in data) {
+            playlist[i] = {
+                id: data[i].resourceId.videoId,
+                artist: this.getArtist(data[i].title),
+                title: this.getTitle(data[i].title)
+            }
+        }
+
+        return playlist;
+    },
+
+    getArtist: function(title) {
+        return title.split(' - ')[0];
+    },
+
+    getTitle: function(title) {
+        return title.split(' - ')[1];
     }
 } 
