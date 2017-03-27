40f5f417b5499e3ffd45faf966bff17de6b398ca
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-03-27 17:39:31 -0400

    subscribe link fades in

--- src/js/modules/navigation.js
+++ src/js/modules/navigation.js
@@ -9,9 +9,25 @@ module.exports =  {
         $('.playlist__hide-button').click(function() {
             this.hidePlaylist();
         }.bind(this));
+
+        $('.header__button').click(function() {
+            this.showPanel();
+        }.bind(this));
+
+        $('.page-fade').click(function() {
+            this.hidePanel();
+        }.bind(this));
     },
 
     hidePlaylist: function() {
         $('body').toggleClass('is-closed');
+    },
+
+    showPanel: function() {
+        $('body').addClass('is-subscribing');
+    },
+
+    hidePanel: function() {
+        $('body').removeClass('is-subscribing');
     }
 };
