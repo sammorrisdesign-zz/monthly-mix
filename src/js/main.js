1eb98965826e3442645d5fca3a92f11a9968e3e4
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-03-27 17:12:02 -0400

    add subscribe button

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 var player = require('./modules/player.js');
 var navigation = require('./modules/navigation.js');
+var subscribe = require('./modules/subscribe.js');
 
 player.init();
 navigation.init();
+subscribe.init();
