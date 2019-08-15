30ed13bed3ec43ea9a1a34520256e5ae25e4c7d4
Sam Morris <mail@sammorrisdesign.co.uk>
2019-08-15 17:37:42 -0400

    actually correct

--- scripts/data/corrections.js
+++ scripts/data/corrections.js
@@ -5,23 +5,20 @@ module.exports = {
         Object.keys(data).forEach(function(playlist) {
             data.playlist = this.correctPlaylist(data[playlist]);
         }.bind(this));
-        console.log(corrections);
+
         return data;
     },
 
     correctPlaylist: function(playlist) {
         playlist.tracks.forEach(track => {
             const correction = corrections[track.id];
-            delete corrections[track.id];
             if (correction) {
                 if (correction.artist) {
-                    // console.log(track.id, correction.artist, track.artist);
+                    track.artist = correction.artist;
                 }
 
                 if (correction.title) {
-                    if (correction.title == track.title) {
-                        console.log(track.id, correction.title, track.title);
-                    }
+                    track.title = correction.title
                 }
             }
         });
