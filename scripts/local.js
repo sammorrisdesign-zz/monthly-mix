29c73033da848160bebf48f8241a356af0418735
Sam Morris <mail@sammorrisdesign.co.uk>
2019-08-16 19:24:56 -0400

    write local watch scripts

---
+++ scripts/local.js
@@ -0,0 +1,45 @@
+const watch = require('node-watch');
+const browserSync = require('browser-sync').create();
+const browserSyncReuseTab = require('browser-sync-reuse-tab')(browserSync);
+const html = require('./compile/html.js');
+const css = require('./compile/css.js');
+const javascript = require('./compile/javascript.js');
+
+browserSync.init({
+    server: './.build',
+    port: 8000,
+    open: false
+}, browserSyncReuseTab);
+
+browserSync.watch('./.build/*.css', (event, file) => {
+    if (event === 'change') {
+        browserSync.reload('*.css');
+    }
+});
+
+console.log('watching');
+
+watch('src', { recursive: true }, (event, file) => {
+    const fileExt = file.substring(file.lastIndexOf('.') + 1);
+
+    console.log(fileExt);
+
+    switch(fileExt) {
+        case 'html':
+            html.init();
+            break;
+
+        case 'sass':
+            css.init();
+            break;
+
+        case 'js':
+            javascript.init();
+            break;
+
+        default:
+            html.init();
+            css.init();
+            javascript.init();
+    }
+})
