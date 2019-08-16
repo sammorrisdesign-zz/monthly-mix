7994e102c81bfd602f1e7b38398a0bb49438923f
Sam Morris <mail@sammorrisdesign.co.uk>
2019-08-16 16:51:07 -0400

    generate JS

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,5 +1,7 @@
 const html = require('./compile/html.js');
 const css = require('./compile/css.js');
+const javascript = require('./compile/javascript.js');
 
 html.init();
 css.init();
+javascript.init();
---
+++ scripts/compile/javascript.js
@@ -0,0 +1,34 @@
+const fs = require('fs-extra');
+const rollup = require('rollup');
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+
+module.exports = {
+    init: function() {
+        let isCompiling = false;
+
+        this.bundle();
+    },
+
+    bundle: function() {
+        const inputOptions = {
+            input: './src/js/main.js',
+            plugins: [
+                resolve(),
+                commonjs()
+            ]
+        };
+
+        const outputOptions = {
+            format: 'iife'
+        };
+
+        (async function() {
+            const bundle = await rollup.rollup(inputOptions);
+            const output = await bundle.generate(outputOptions);
+            fs.mkdirsSync('.build/assets/js');
+            console.log(output.output);
+            fs.writeFileSync('.build/assets/js/main.js', output.output[0].code);
+        })();
+    }
+}
