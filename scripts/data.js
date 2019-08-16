8cc5de587100e40fe15e0a7e1ccc786891237f99
Sam Morris <mail@sammorrisdesign.co.uk>
2019-08-15 21:18:57 -0400

    clean up data.js file

--- scripts/data.js
+++ scripts/data.js
@@ -6,9 +6,7 @@ const corrections = require('./data/corrections.js');
 let data = playlists.init();
     data = tracks.init(data);
     data = corrections.init(data);
-// Todo: Corrections
-
-console.log('Writing data file');
 
 fs.writeFileSync('data.json', JSON.stringify(data));
 
+console.log('Data updated!');
