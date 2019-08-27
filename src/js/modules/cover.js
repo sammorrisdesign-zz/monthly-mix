8c60452fa8c1438e32bbe2197157e1aafc3ccd6b
Sam Morris <mail@sammorrisdesign.co.uk>
2019-08-27 13:13:06 -0400

    use dom elements instead

--- src/js/modules/cover.js
+++ src/js/modules/cover.js
@@ -1,16 +1,13 @@
 import Matter from 'matter-js';
-import pathseg from 'pathseg'
-import decomp from 'poly-decomp'
-window.decomp = decomp;
 
 const renderCover = () => {
+    let isRunning = true;
     // module aliases
     const Engine = Matter.Engine,
         Render = Matter.Render,
         World = Matter.World,
         Bodies = Matter.Bodies,
-        Body = Matter.Body,
-        Svg = Matter.Svg;
+        Body = Matter.Body;
 
     // create an engine
     const engine = Engine.create();
@@ -23,50 +20,70 @@ const renderCover = () => {
             height: window.innerHeight,
             width: window.innerWidth,
             wireframeBackground: 'transparent',
-            background: 'transparent',
-            wireframes: false
+            background: 'transparent'
         }
     });
 
     const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
-    const vertexSets = [];
-    const month = 'may';
-    const characters = month.split('');
-    const ids = characters.map(character => { return `#${character}` }).join();
-
-    console.log(ids);
-    const svg = document.querySelectorAll(ids);
-    svg.forEach((node, i) => {
-        const v = Bodies.fromVertices(100+(i*150), 200, Svg.pathToVertices(node, 1), {
-            render: {
-              fillStyle: '#ffffff',
-              strokeStyle: '#ffffff',
-              lineWidth: 1
-            },
-            frictionAir: 0.05,
-            mass: 20
-          }, true, 0);
-
-        Body.setAngularVelocity(v, 0.02);
-        Body.applyForce(v, v.position, {
-            x: 0.01,
-            y: 0.02
-        });
-
-        vertexSets.push(v);
-    })
-
-    engine.world.gravity.scale = 0.000001;
-
     // add all of the bodies to the world
-    World.add(engine.world, vertexSets);
+    World.add(engine.world, [ground]);
 
     // run the engine
     Engine.run(engine);
 
+    const elements = document.querySelectorAll('.is-movable');
+    let bodies = [];
+
+    elements.forEach((el, i) => {
+        el.setAttribute('data-left', el.offsetLeft + (el.offsetWidth / 2));
+        el.setAttribute('data-top', el.offsetTop + (el.offsetHeight / 2));
+        const body = Bodies.rectangle(
+            el.offsetLeft + (el.offsetWidth / 2), el.offsetTop + (el.offsetHeight / 2), el.offsetWidth, el.offsetHeight, {
+                frictionAir: 0.05,
+                mass: 20
+            }
+        );
+
+        Body.setAngularVelocity(body, Math.random() * 0.1 - 0.05);
+        Body.applyForce(body, body.position, {
+            x: Math.random() * 0.1 - 0.05,
+            y: Math.random() * 0.1 - 0.05
+        });
+
+        el.id = body.id;
+        bodies.push(body);
+    });
+
+    World.add(engine.world, bodies);
+
+    engine.world.gravity.scale = 0.000001;
+
+    const update = () => {
+        elements.forEach((el, i) => {
+            let body = null;
+            bodies.forEach((b, id) => {
+                if (b.id == el.id) {
+                    body = b;
+                }
+            });
+
+            el.style.transform = `translateX(${body.position.x - el.getAttribute('data-left')}px) translateY(${body.position.y - el.getAttribute('data-top')}px) rotate(${body.angle}rad)`;
+        });
+        if (isRunning) {
+            window.requestAnimationFrame(update);
+        }
+    }
+
+    window.requestAnimationFrame(update);
+
     // run the renderer
     Render.run(render);
+
+    setTimeout(() => {
+        Render.stop(render);
+        isRunning = false;
+    }, 1000);
 }
 
 export default {
